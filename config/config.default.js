e851f2416912aa67062e6d5ae373668ae1ee8a27
何东升 <he77505357@163.com>
2021-03-09 08:53:07 +0800

    feat: 跨域

--- config/config.default.js
+++ config/config.default.js
@@ -10,7 +10,18 @@ module.exports = appInfo => {
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+  const config = exports = {}
+  config.security = {
+    csrf: {
+      enable: false,
+      ignoreJSON: true
+    },
+    domainWhiteList: [ '*' ], // 白名单
+  };
+  config.cors = {
+    origin: '*', // 如果不写origin则会按照白名单中的域名允许跨域， * 代表允许所有的域名进行跨域请求
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
+  }
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1614326916617_6886';
